7ebca9ef3ea43a0c1fccb730da08683275ac48e9
Smorgan1995 <morgan-s20@ulster.ac.uk>
2026-03-20 15:21:10 +0000

    add guess counter functionality
    -define variable to hold guess count
    -add guess count + 1 when input given
    -edit print statement to include number of guesses taken

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -1,18 +1,20 @@
-# import random
+import random
 
 # pick a random number for the user to guess
-rand = random.randint(1, 100)
-
+rand = random.randint(1, 20)
+number_of_guesses = 0
 print('Guess a number between 1 and 20.')
-guess = int(input())  # number needs to be an integer
+guess = int(input())# number needs to be an integer
+number_of_guesses += 1
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
-        print('Too low. Guess again.')
-    else:  # if the guess is too low, tell the user.
         print('Too high. Guess again.')
+    else:  # if the guess is too low, tell the user.
+        print('Too low. Guess again.')
 
     print('Enter a new guess: ')
-    guess = input()
+    guess = int(input())
+    number_of_guesses += 1
 
-print('You got it! The number was {}'.format(rand))
+print('You got it! The number was {} and you took {} to guess correctly!'.format(rand, number_of_guesses))
